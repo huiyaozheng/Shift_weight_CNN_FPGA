8869a388880696c3886c8401520690b96984f221
Alex Zheng <huiyaozheng@users.noreply.github.com>
2018-11-22 23:24:26 +0000

    Updated license

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Shift_weight_CNN_FPGA
+# Shift weight CNNs for FPGAs
+
+- Tested with MNIST, 30x speedup compared to CPU
+
+- Intel OpenCL FPGA SDK 16.1; Visual Studio 2012
--- host/readMnist.cpp
+++ host/readMnist.cpp
@@ -1,3 +1,20 @@
+// Copyright (C) 2018 Huiyao Zheng
+// 
+// This file is part of Shift_weight_CNN_FPGA.
+// 
+// Shift_weight_CNN_FPGA is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Shift_weight_CNN_FPGA is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Shift_weight_CNN_FPGA.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef FSTREAM
 #define FSTREAM
 #include<fstream>
